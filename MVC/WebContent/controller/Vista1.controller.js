e9632a29ce9c074e4875e642bef1c6536e2d83c7
Aaron Juarez <2963595050101@ingenieria.usac.edu.gt>
2021-03-29 16:17:59 -0600

    resources

--- MVC/WebContent/controller/Vista1.controller.js
+++ MVC/WebContent/controller/Vista1.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
 ], function (Controller) {
     "use restrict";
-    return Controller.extend("controller.Vista1",{
+    return Controller.extend("sap.ui.demo.wt.controller.Vista1",{
 
         holaMundo: function () {
             alert("Hola Mundo");
